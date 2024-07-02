f2a5ec786a5f31773486265d2c78031e85bd7750
Александр Леонов <leonov.alexandr@gmail.com>
2024-07-02 15:41:03 +0300

    10 topic - done

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+# Импортируем функцию, позволяющую серверу разработки отдавать файлы.
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-]
+# В конце добавляем к списку вызов функции static.
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
